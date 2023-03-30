b868ee52999305bb49f6f690b18a29277e9ae5cd
Nicho001 <nicholassoman01@gmail.com>
2023-03-30 22:31:30 +0530

    update

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const productRouter = express.Router();
 const Shop = require("../models/shop");
+const FinalBill = require("../models/finalbill");
 
 productRouter.get("/products", async (req, res) => {
   try {
     const products = await Shop.find();
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+productRouter.get("/billhistory", async (req, res) => {
+  try {
+    const products = await FinalBill.find();
     res.status(200).json(products);
   } catch (e) {
     res.status(500).json({ error: e.message });
